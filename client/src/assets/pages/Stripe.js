a1f073604177bdc1a1e575934fa7d9fbd158b5ae
Breedlove13 <brianbreedlove0013@gmail.com>
2021-07-05 13:44:16 -0700

    stripe front end addded

--- client/src/assets/pages/Stripe.js
+++ client/src/assets/pages/Stripe.js
@@ -1,5 +1,19 @@
 import React from "react";
-import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+import "../components/Stripe/stripe.css";
 
+
+const promise = loadStripe("pk_test_51J9IXHF9OL10HIOgtFO0mvMGTJDS7Krs90HSunOA1AmT4JbWjupZfezsukkcX16dIa4MGExddp88AYBYtVphtJon00ZY3UBqfO");
+
+export default function App() {
+  return (
+    <div className="App">
+      <Elements stripe={promise}>
+        <CheckoutForm />
+      </Elements>
+    </div>
+  );
+}
 
